98106f2543ce72f675464f524a383a646d709fc3
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2023-08-17 18:45:19 +0200

    implementing "smooth scrolling"

--- js/script.js
+++ js/script.js
@@ -1,31 +1,67 @@
 const yearEl = document.querySelector(".year");
-yearEl.textContent= new Date().getFullYear();
+yearEl.textContent = new Date().getFullYear();
 
+
+
+// mobile-navigation:
 const menuButtonEl = document.querySelector(".btn-mobile-nav");
-menuButtonEl.addEventListener("click" , function () {
-  document.querySelector(".header").classList.toggle("nav-open");
+menuButtonEl.addEventListener("click", function () {
+    document.querySelector(".header").classList.toggle("nav-open");
 });
 
+
+const menuItemsEl = document.querySelectorAll(".main-nav-link");
+menuItemsEl.forEach(function (item) {
+  item.addEventListener("click" , function () {
+    document.querySelector(".header").classList.remove("nav-open");
+  })
+});
+
+
+
+// smooth scrolling animation:
+// const allLinks = document.querySelectorAll("a:link");
+//
+// allLinks.forEach(function (link) {
+//     link.addEventListener("click", function (e) {
+//         e.preventDefault();
+//         const href = link.getAttribute("href");
+//
+//         if (href === "#") {
+//             window.scrollTo({
+//                 top: 0,
+//                 behavior: "smooth",
+//             });
+//         }
+//         if (href !== "#" && href.startsWith("#")) {
+//             const sectionEl = document.querySelectorAll(href);
+//             sectionEl.scrollIntoView({
+//                 behavior: "smooth"
+//             })
+//         }
+//     })
+// });
 
 
 //////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
-  var flex = document.createElement("div");
-  flex.style.display = "flex";
-  flex.style.flexDirection = "column";
-  flex.style.rowGap = "1px";
+    var flex = document.createElement("div");
+    flex.style.display = "flex";
+    flex.style.flexDirection = "column";
+    flex.style.rowGap = "1px";
 
-  flex.appendChild(document.createElement("div"));
-  flex.appendChild(document.createElement("div"));
+    flex.appendChild(document.createElement("div"));
+    flex.appendChild(document.createElement("div"));
 
-  document.body.appendChild(flex);
-  var isSupported = flex.scrollHeight === 1;
-  flex.parentNode.removeChild(flex);
-  console.log(isSupported);
+    document.body.appendChild(flex);
+    var isSupported = flex.scrollHeight === 1;
+    flex.parentNode.removeChild(flex);
+    console.log(isSupported);
 
-  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+    if (!isSupported) document.body.classList.add("no-flexbox-gap");
 }
+
 checkFlexGap();
 
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
